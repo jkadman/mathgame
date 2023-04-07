9194f6dea840076e98b30ce6d9129bb0f7e7b744
Jason Kadman <jkadman231@gmail.com>
2023-04-07 00:00:47 +0000

    add new module for checking truthiness of question

--- Questions.rb
+++ Questions.rb
@@ -1,10 +1,23 @@
 
+
 module Math_Questions
   def question
     @num1 = rand(1..20)
     @num2 = rand(1..20)
     puts "what is #{@num1} + #{@num2}?"
+    @answer = @num1 + @num2
+    return @answer
   end
 end
 
+module Check_Questions
+  def check_answer(answer, num)
+    # num1 = question
+    if answer == num
+      true
+    else
+      false
+    end
+  end
+end
 
