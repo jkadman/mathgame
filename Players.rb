c66a49666f82b802b0d1982fb263a693bac551e0
Jason Kadman <jkadman231@gmail.com>
2023-04-09 10:03:44 +0000

    add welcome message

--- Players.rb
+++ Players.rb
@@ -1,14 +1,32 @@
 class Players
-  def initialize(name1, name2)
-    @Player1 = name1
-    @Player2 = name2
-    @player1_points = 3
-    @player2_points = 3
+  attr_accessor :player1_points, :player2_points
+
+  puts "Hello new Players please enter your names"
+  def initialize
+    @Player = gets.chomp
+    # @Player2 = "Player 2"
+    @player_points = 3
+    # @player2_points = 3
   end
 
   def to_s
-    "Hello #{@Player1} welcome to the game.  Hello #{@Player2} welcome to the game."
+    "#{@Player}"
+    # @Player2 = "Player 2"
   end
+
+  # def Player1
+  #   @Player1
+  # end
+
+  # def Player2
+  #   @Player2
+  # end
+
+  def player_points
+    @player_points
+  end
+
+  
 
 end
 
@@ -19,3 +37,10 @@ end
 
 # Jason = Players.new(gets.chomp, gets.chomp)
 # puts Jason
+
+
+
+#should use getter and setter to get/set name
+
+# t = Players.new 'Jason', 'eddit'
+# p t.player1_points
